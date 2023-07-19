9a23606315fdb2569b3723730905a7f010128780
Libovski <goncharov199306@gmail.com>
2023-07-19 20:20:20 +0700

    add ship movement, left and right

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -1,6 +1,16 @@
 public class SpaceShip extends BaseObject {
 
-    public SpaceShip(double x, double y, double radius) {
-        super(x, y, radius);
+    private double dx = 0;
+
+    public SpaceShip(double x, double y) {
+        super(x, y, 3);
+    }
+
+    public void moveLeft(){
+        dx = -1;
+    }
+
+    public void moveRight(){
+        dx = 1;
     }
 }
