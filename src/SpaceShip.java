ad0a3023e941451f948cb9b211c90b3f7fb03595
Libovski <goncharov199306@gmail.com>
2023-07-19 21:10:56 +0700

    add in space ship two methods, move and draw

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -1,16 +1,31 @@
 public class SpaceShip extends BaseObject {
 
-    private double dx = 0;
+    private double dx;
 
     public SpaceShip(double x, double y) {
         super(x, y, 3);
     }
 
-    public void moveLeft(){
+    public void moveLeft() {
         dx = -1;
     }
 
-    public void moveRight(){
+    public void moveRight() {
         dx = 1;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+    }
+
+    @Override
+    public void move() {
+        x = x + dx;
+        checkBorders(radius, Space.game.getWidth() - radius + 1, 1, Space.game.getHeight() + 1);
+    }
+
+    public void fire() {
+        Space.game.getRockets().add(new Rocket(x - 2, y));
+        Space.game.getRockets().add(new Rocket(x + 2, y));
     }
 }
